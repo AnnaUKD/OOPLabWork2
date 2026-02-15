fd0abbcaab3571df016b81e4a28410bfc04dea70
PogoreltsevaAnnaUKDIPZ <anna.i.pohoreltseva@ukd.edu.ua>
2026-02-15 17:43:27 +0200

    finished full lab work

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,15 +1,15 @@
 import java.text.DecimalFormat;
 
 public class BankAccount {
+    private static int nextAccountNumber = 100000;
     private String accountNumber;
     private String ownerName;
     private double balance;
 
-
+    // Конструктор з номером рахунку
     public BankAccount(String accountNumber, String ownerName, double balance) {
         this.accountNumber = accountNumber;
         setOwnerName(ownerName);
-
         if (balance >= 0) {
             this.balance = balance;
         } else {
@@ -18,6 +18,21 @@ public class BankAccount {
         }
     }
 
+    // Конструктор без номера рахунку
+    public BankAccount(String ownerName, double balance) {
+        this.accountNumber = generateAccountNumber();
+        setOwnerName(ownerName);
+        if (balance >= 0) {
+            this.balance = balance;
+        } else {
+            this.balance = 0;
+            System.out.println("Баланс не може бути від'ємним. Встановлено 0.");
+        }
+    }
+
+    private String generateAccountNumber() {
+        return "UA" + (nextAccountNumber++);
+    }
 
     public String getAccountNumber() {
         return accountNumber;
@@ -31,23 +46,24 @@ public class BankAccount {
         return balance;
     }
 
-
     public void setOwnerName(String newOwnerName) {
-        if (newOwnerName.length() >= 3) {
+        if (newOwnerName != null && newOwnerName.length() >= 3) {
             this.ownerName = newOwnerName;
         } else {
             System.out.println("Ім'я власника має містити щонайменше 3 символи.");
+            // Якщо ownerName ще null (при створенні), встановлюємо значення за замовчуванням
+            if (this.ownerName == null) {
+                this.ownerName = "Невідомий";
+            }
         }
     }
 
-
-    public void getAccountInfo(){
+    public void getAccountInfo() {
         DecimalFormat df = new DecimalFormat("0.00");
         System.out.println("Номер рахунку: " + getAccountNumber());
         System.out.println("Власник: " + getOwnerName());
         System.out.println("Баланс: " + df.format(getBalance()));
     }
-
 
     public void deposit(double amount) {
         if (amount > 0) {
@@ -57,7 +73,6 @@ public class BankAccount {
         }
     }
 
-
     public void withdraw(double amount) {
         if (amount <= 0) {
             System.out.println("Ви не можете зняти нульову або від'ємну суму.");
@@ -66,8 +81,5 @@ public class BankAccount {
         } else {
             balance -= amount;
         }
-
-
-
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,102 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount("12345614", "Оа", 1000.5);
-        BankAccount account1 = new BankAccount("3421414", "Андрій", 50000.5);
-        BankAccount account2 = new BankAccount("8569621", "Богдан", 20000.5);
-        BankAccount account3 = new BankAccount("8749585", "Мілена", 100000.5);
-        BankAccount account4 = new BankAccount("6859608", "Василь", 2000.5);
-        BankAccount account5 = new BankAccount("8904167", "Лілія", 15000.0);
+        // Створюємо рахунки (виправлено "Оа" на "Олександр")
+        BankAccount account = new BankAccount("Олександр", 1000.5);
+        BankAccount account1 = new BankAccount("Андрій", 50000.5);
+        BankAccount account2 = new BankAccount("Богдан", 20000.5);
+        BankAccount account3 = new BankAccount("Мілена", 100000.5);
+        BankAccount account4 = new BankAccount("Василь", 2000.5);
+        BankAccount account5 = new BankAccount("Лілія", 15000.0);
 
+        // Створюємо масив усіх рахунків
+        BankAccount[] accounts = new BankAccount[]{
+                account, account1, account2, account3, account4, account5
+        };
 
         account.getAccountInfo();
-
-        account.deposit(250.75f);
+        account.deposit(250.75);
         account.getAccountInfo();
-        account.withdraw(300f);
+        account.withdraw(300);
         account.getAccountInfo();
         account.setOwnerName("Ол");
         account.getAccountInfo();
-
-        account.setOwnerName("Олена-Анна");
+        account.setOwnerName("Олексій");
         account.getAccountInfo();
 
+        account1.getAccountInfo();
+        account1.deposit(250.75);
+        account1.getAccountInfo();
+        account1.withdraw(300);
+        account1.getAccountInfo();
+        account1.setOwnerName("Ен");
+        account1.getAccountInfo();
+        account1.setOwnerName("Ендрю");
+        account1.getAccountInfo();
+
+        account2.getAccountInfo();
+        account2.deposit(250.75);
+        account2.getAccountInfo();
+        account2.withdraw(300);
+        account2.getAccountInfo();
+        account2.setOwnerName("Б");
+        account2.getAccountInfo();
+        account2.setOwnerName("Богдан-Пантеон");
+        account2.getAccountInfo();
+
+        account3.getAccountInfo();
+        account3.deposit(250.75);
+        account3.getAccountInfo();
+        account3.withdraw(300);
+        account3.getAccountInfo();
+        account3.setOwnerName("Мл");
+        account3.getAccountInfo();
+        account3.setOwnerName("Мілен");
+        account3.getAccountInfo();
+
+        account4.getAccountInfo();
+        account4.deposit(250.75);
+        account4.getAccountInfo();
+        account4.withdraw(300);
+        account4.getAccountInfo();
+        account4.setOwnerName("Ва");
+        account4.getAccountInfo();
+        account4.setOwnerName("Василій");
+        account4.getAccountInfo();
+
+        account5.getAccountInfo();
+        account5.deposit(250.75);
+        account5.getAccountInfo();
+        account5.withdraw(300);
+        account5.getAccountInfo();
+        account5.setOwnerName("Лі");
+        account5.getAccountInfo();
+        account5.setOwnerName("Ліліян");
+        account5.getAccountInfo();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nВведіть номер рахунку для пошуку:");
+        String searchNumber = scanner.nextLine();
+
+        BankAccount foundAccount = findAccountByNumber(accounts, searchNumber);
+        if (foundAccount != null) {
+            System.out.println("Рахунок знайдено:");
+            foundAccount.getAccountInfo();
+        } else {
+            System.out.println("Рахунок з номером " + searchNumber + " не знайдено.");
+        }
+
+        scanner.close();
     }
 
+    public static BankAccount findAccountByNumber(BankAccount[] accounts, String accountNumber) {
+        for (BankAccount acc : accounts) {
+            if (acc.getAccountNumber().equals(accountNumber)) {
+                return acc;
+            }
+        }
+        return null;
+    }
 }
 
